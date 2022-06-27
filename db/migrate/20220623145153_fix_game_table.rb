7d20613a021d6f7be5450b0e9572176669e3cb0d
Madeleine Pla <madeleine.pla7@gmail.com>
2022-06-27 11:08:39 -0400

    Fix for heroku

--- db/migrate/20220623145153_fix_game_table.rb
+++ db/migrate/20220623145153_fix_game_table.rb
@@ -1,8 +1,8 @@
 class FixGameTable < ActiveRecord::Migration[5.2]
   def change
-    remove_column :games, :category
-    remove_column :games, :mechanisms
-    remove_column :games, :awards
+    # remove_column :games, :category
+    # remove_column :games, :mechanisms
+    # remove_column :games, :awards
 
     add_column :games, :header_img, :string, null: false
   end
