f050b61343463c9943f518ad6cefdc40a1d57fae
Madeleine Pla <madeleine.pla7@gmail.com>
2022-06-29 13:44:20 -0400

    Item like updates state

--- frontend/components/list_item/list_item.jsx
+++ frontend/components/list_item/list_item.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThumbsUp } from '@fortawesome/free-regular-svg-icons';
 import { useHistory } from "react-router-dom";
 
 const ListItem = ({ order, item, author, currentUserId, deleteListItem, likeItem, unlikeItem }) => {
+  const [liked, updateLike] = useState(item.liked_by_current_user);
+  
+  useEffect(() => {
+    // debugger
+    const likeBtn = document.getElementById('like-btn')
+    console.log(liked)
+    console.log(likeBtn)
+    if (liked) {
+      // likeBtn.classList.add("liked")
+      // likeBtn.style.color = 'blue'
+      console.log(likeBtn)
+    } else {
+      // likeBtn.classList.remove("liked")
+      // likeBtn.style.color = 'black'
+      console.log(likeBtn)
+    }
+    
+    // debugger
+  },[liked]);
+
   let bodyParagraphs = []
   if (item.body) {
     bodyParagraphs = item.body.split("\n")
@@ -16,15 +36,14 @@ const ListItem = ({ order, item, author, currentUserId, deleteListItem, likeItem
   let likeButtonAction;
   if (currentUserId) {
     if (item.liked_by_current_user) {
-      // debugger;
       likeButtonAction = () => {
         return unlikeItem(item.id)
-          .then(() => (history.go(0)))
+          .then(() => updateLike(!liked))
       }
     } else {
       likeButtonAction = () => {
         return likeItem(item.id)
-          .then(() => (history.go(0)))
+          .then(() => updateLike(!liked))
       }
     }
   } else {
@@ -78,14 +97,14 @@ const ListItem = ({ order, item, author, currentUserId, deleteListItem, likeItem
     {
       author.id === currentUserId ?
       <div className='list-item-btns'>
-        <button onClick={likeButtonAction} className={item.liked_by_current_user ? 'liked' : ''}>
+        <button onClick={likeButtonAction} id="like-btn" className={item.liked_by_current_user ? 'liked' : ''}>
           <FontAwesomeIcon icon={faThumbsUp} /> | {item.likes}
         </button>
         <button className='list-item-edit-btn'><Link to={`/lists/${item.list_id}/list_items/${item.id}/edit`}>Edit</Link></button>
         <button className='list-item-delete-btn' onClick={() => handleDelete()}>Delete</button>
       </div> :
       <div className='list-item-btns'>
-        <button onClick={likeButtonAction} className={item.liked_by_current_user ? 'liked' : ''}>
+          <button onClick={likeButtonAction} id="like-btn" className={item.liked_by_current_user ? 'liked' : ''}>
           <FontAwesomeIcon icon={faThumbsUp} /> | {item.likes}
         </button>
       </div>
